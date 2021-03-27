f9d12d3980877bbdbb937890c796f727dabcf9a7
angelbeats122 <kyujitsu@icloud.com>
2021-03-27 18:45:23 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.oeder(id: "DESC")
   end
 
-  def new
-  end  
+  
 
   def create
   
     Post.create(content: params[:content])
+    redirect_tp actin: :index
   end
 end
